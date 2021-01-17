de676d3228a19422a3656f442c8ac13281c19ca8
Markus Monz <markusmonz@googlemail.com>
2021-01-17 19:35:37 +0100

    cleanup: remove project specific code keep common code

--- setup.py
+++ setup.py
@@ -3,15 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='ham10000_utils',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.0',
     description='Util package for HAM10000 dataset',
     author='Markus Monz',
-    install_requires = [
+    install_requires=[
         'pandas >= 1.1.4',
     ],
-    extras_require={
-        "tf": ["tensorflow >= 2.3.1"],
-        "tf_gpu": ["tensorflow-gpu>=2.3.1"],
-    },
     license='MIT',
 )
